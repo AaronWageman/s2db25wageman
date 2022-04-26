519dba4e3e8a579679a0fc945875ef2d066a6683
AaronWageman <69913293+AaronWageman@users.noreply.github.com>
2022-04-26 18:50:13 -0500

    Added validator

--- models/entree.js
+++ models/entree.js
@@ -3,9 +3,9 @@ const entreeSchema = mongoose.Schema({
  name: String, 
  calories: Number, 
  cost:{
-    //type: Number,
-    //min: [1, 'Cost must be more than 1'],
-    //max: 1000
+    type: Number,
+    min: [1, 'Cost must be more than 1'],
+    max: 1000
  }
 }); 
  
